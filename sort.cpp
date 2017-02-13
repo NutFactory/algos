0f12094252366dda6cc2072691c16af4800fd22d
NutFactory <thatshaley@hotmail.com>
2017-02-13 11:53:16 -0400

    Selection Sort

--- sort.cpp
+++ sort.cpp
@@ -21,7 +21,7 @@ int main()
 {
   vector<string> inputs;
   string search_key, input;
-  int result;
+ 
 
    cout<<"Welcome to \"search it\". We first need some input data."<<endl;
    cout<<"We'll assume the inputs do not have any spaces."<<endl<<endl;
@@ -42,37 +42,13 @@ int main()
       cout<<endl<<"No input received, quiting..."<<endl<<endl;
        exit(1);//nothing to do but quit program
   }
- 
-   cout<<endl<<"To end input type the #-character (followed by Enter)"<<endl<<endl;
-  cout<<"Enter a value to search for: ";
 
-
-   cin>>search_key;
- 
-    while(search_key != "#")//perform searches until sentinel entered
-    {
-        result = linearSearch(inputs,search_key);
-
-        cout<<"  '"<<search_key<<"' was ";
-
-        if (result == -1)
-          cout<<"not found";
-        else
-          cout<<"found at index "<<result;
-
-
-        cout<<endl<<endl<<"Enter a value to search for: ";
-        cin>>search_key; 
-    }
-
-   cout<<endl<<"Program \"search it\" is now finished."<<endl<<endl;
-
-	if(inputs.size() == 1000000){	
+	
 	selectionSort(inputs);
-}
 	displayData(inputs);
     return 0;
 }
+
 int linearSearch(auto data, auto key)
 {
 	for (int i=0; 1 < data.size(); i++)
